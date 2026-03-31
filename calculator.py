e1148c401e92c5854d9fe703db02543066ee35ed
dilumBM <dilum.rathnayake@blockmajority.io>
2026-03-31 15:42:44 +1100

    feat: Create a simple Python calculator

--- calculator.py
+++ calculator.py
@@ -1,13 +1,14 @@
-def add(a, b):
-    return a + b
+class Calculator:
+    def add(self, a, b):
+        return a + b
 
-def subtract(a, b):
-    return a - b
+    def subtract(self, a, b):
+        return a - b
 
-def multiply(a, b):
-    return a * b
+    def multiply(self, a, b):
+        return a * b
 
-def divide(a, b):
-    if b == 0:
-        raise ValueError("Cannot divide by zero")
-    return a / b
+    def divide(self, a, b):
+        if b == 0:
+            raise ValueError("Cannot divide by zero")
+        return a / b

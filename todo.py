982463151675c30eaff7337370a2885cba57a560
shreyas-vivek <shreyasvivek01@gmail.com>
2026-04-09 14:26:52 +1000

    fix: address CodeRabbit comments

--- todo.py
+++ todo.py
@@ -5,7 +5,7 @@ def add_task(task_list, task):
 
 def remove_task(task_list, task):
     for index, item in enumerate(task_list):
-        if "task" in item and item["task"] == task:
+        if isinstance(item, dict) and item.get("task") == task:
             del task_list[index]
             break
     return task_list
@@ -13,7 +13,7 @@ def remove_task(task_list, task):
 
 def complete_task(task_list, task):
     for item in task_list:
-        if "task" in item and item["task"] == task:
+        if isinstance(item, dict) and item.get("task") == task:
             item["completed"] = True
             break
     return task_list

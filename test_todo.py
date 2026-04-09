2c595729f882ba97de35161a7645e7318797b55b
shreyas-vivek <shreyasvivek01@gmail.com>
2026-04-09 13:56:03 +1000

    feat: add simple to-do list app

---
+++ test_todo.py
@@ -0,0 +1,34 @@
+import unittest
+
+from todo import add_task, remove_task, complete_task, list_tasks
+
+
+class TestTodo(unittest.TestCase):
+    def test_add_task_adds_a_task_correctly(self):
+        task_list = []
+        add_task(task_list, "Buy milk")
+        self.assertEqual(task_list, [{"task": "Buy milk", "completed": False}])
+
+    def test_remove_task_removes_a_task_correctly(self):
+        task_list = [
+            {"task": "Buy milk", "completed": False},
+            {"task": "Walk dog", "completed": False},
+        ]
+        remove_task(task_list, "Buy milk")
+        self.assertEqual(task_list, [{"task": "Walk dog", "completed": False}])
+
+    def test_complete_task_marks_task_as_completed(self):
+        task_list = [{"task": "Buy milk", "completed": False}]
+        complete_task(task_list, "Buy milk")
+        self.assertEqual(task_list, [{"task": "Buy milk", "completed": True}])
+
+    def test_list_tasks_returns_all_tasks(self):
+        task_list = [
+            {"task": "Buy milk", "completed": False},
+            {"task": "Walk dog", "completed": True},
+        ]
+        self.assertEqual(list_tasks(task_list), task_list)
+
+
+if __name__ == "__main__":
+    unittest.main()
---
+++ todo.py
@@ -0,0 +1,23 @@
+def add_task(task_list, task):
+    task_list.append({"task": task, "completed": False})
+    return task_list
+
+
+def remove_task(task_list, task):
+    for index, item in enumerate(task_list):
+        if item["task"] == task:
+            del task_list[index]
+            break
+    return task_list
+
+
+def complete_task(task_list, task):
+    for item in task_list:
+        if item["task"] == task:
+            item["completed"] = True
+            break
+    return task_list
+
+
+def list_tasks(task_list):
+    return task_list
